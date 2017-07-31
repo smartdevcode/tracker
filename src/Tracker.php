bc927960b2f3956c952e7c3f7c19e1f88f71ce46
SeniorDevCode <senior.dev.software@gmail.com>
2017-07-31 03:37:10 +0200

    fixed error (stat panel uses created_at but select takes updated_at)

--- src/Tracker.php
+++ src/Tracker.php
@@ -283,7 +283,6 @@ class Tracker
     {
         return $this->config->get('enabled') &&
                 $this->logIsEnabled() &&
-                $this->notConsole() &&
                 $this->parserIsAvailable() &&
                 $this->isTrackableIp() &&
                 $this->isTrackableEnvironment() &&
@@ -398,11 +397,6 @@ class Tracker
     public function pageViewsByCountry($minutes, $results = true)
     {
         return $this->dataRepositoryManager->pageViewsByCountry(Minutes::make($minutes), $results);
-    }
-
-    public function notConsole()
-    {
-        return $this->laravel->runningInConsole();
     }
 
     public function parserIsAvailable()
